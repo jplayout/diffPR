f74a7af2f44aa1dd85a686bbc369144a9a3ca7c7
Jeremie PLAYOUT <jplayout@cloudbees.com>
2024-12-13 13:18:32 +0100

    fix

--- BEE-42296/pipeline.groovy
+++ BEE-42296/pipeline.groovy
@@ -3,15 +3,11 @@
 def days = 5
 
 stage("Get RFC stages stats") {
-    podTemplate (cloud: 'kubernetes', yaml: readTrusted('nodes/metricsPodTemplateDefinition')) {
-        retry(count: 2, conditions: [kubernetesAgent(), nonresumable()]) {
             node() {
                 withCredentials([usernamePassword(credentialsId: '7-League-API-Query', usernameVariable: 'USER_NAME', passwordVariable: 'API_TOKEN')]) {
                     checkout scm
                     sh 'DAYS_OLD=${days} BEE-42296/build_time_metrics.sh'
                     archiveArtifacts artifacts: 'stages.csv'
                  }
-            }
-        }
     }
 }
